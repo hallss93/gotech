ea3a1b4890d3baaeb7fd2c19c519a506ed5258ac
Hallison Melo <hallss93@hotmail.com>
2020-05-22 15:30:23 -0300

    Navbar dots

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,10 @@
     <div class="c-container c-container-flex ml-60 mr-60">
       <img src="@/assets/logo.svg" />
       <ul class="c-list">
-        <li v-for="(item, index) in menu" :key="index">{{ item.name }}</li>
+        <li v-for="(item, index) in menu" :key="index">
+          <p v-bind:class="{ active: item.active }">{{ item.name }}</p>
+          <p v-if="item.active" class="c-dot"></p>
+        </li>
       </ul>
       <div class="c-form-group">
         <input type="text" placeholder="Search Here" class="c-input" />
@@ -23,6 +26,12 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class NavBar extends Vue {
-  menu = [{ name: "Home" }, { name: "Home" }, { name: "Home" }];
+  menu = [
+    { name: "Home", active: true },
+    { name: "About", active: false },
+    { name: "Product", active: false },
+    { name: "Career", active: false },
+    { name: "Blog", active: false }
+  ];
 }
 </script>
